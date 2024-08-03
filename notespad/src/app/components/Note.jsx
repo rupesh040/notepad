a12ee0d3bc10e43b2b6ace00f88df0860ee000d7
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-08-03 21:55:22 +0530

    complte

--- notespad/src/app/components/Note.jsx
+++ notespad/src/app/components/Note.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-const Note = () => {
+import React, { useRef } from 'react'
+import { motion } from "framer-motion"
+const Note = ({text,id,deleteNote,title,constraintsRef}) => {
   return (
-    <div className="noteCard z-30 w-[300px] h-[300px]  p-3 rounded-2xl overflow-hidden" >
+    <motion.div className="noteCard z-30 w-[300px] h-[300px]  p-3 rounded-2xl overflow-hidden"  drag
+    dragConstraints={constraintsRef}>
         <div className="top flex justify-between items-center pb-1 ">
-        <input type="text" className='text-black text-[20px] w-[80%] outline-none bg-transparent' autoFocus placeholder='Title...'/>
-        <i class="ri-close-fill text-[20px] cursor-pointer" ></i>
+        <input type="text" className='text-black font-bold capitalize text-[20px] w-[80%] outline-none bg-transparent' autoFocus placeholder='Title...' defaultValue={title}/>
+        <i class="ri-close-fill text-[20px] cursor-pointer" onClick={()=>{
+            deleteNote(id)
+        }}></i>
         </div>
 
-        <textarea name="" id="" className='bg-transparent text-black  w-[100%] h-[85%] outline-none pt-2' placeholder='Write here description'></textarea>
-    </div>
+        <textarea name="" id="" className='bg-transparent text-black  w-[100%] h-[85%] outline-none pt-2' placeholder='Write here description' defaultValue={id}></textarea>
+    </motion.div>
   )
 }
 
--- notespad/src/app/page.js
+++ notespad/src/app/page.js
@@ -2,37 +2,34 @@
 import Image from "next/image";
 import 'remixicon/fonts/remixicon.css'
 import Note from "./components/Note";
-import { useEffect, useState } from "react";
-import { stringify } from "postcss";
+import {  useRef, useState } from "react";
 
 export default function Home() {
-    const [note,setNote]=useState([]);
-    useEffect(()=>{
-        const saveNote = localStorage.getItem('note');
-        if(saveNote){
-            setNote(JSON.parse(saveNote));
-        }
-    },[])
-
-    // useEffect(()=>{
-    //     localStorage.setItem('note',JSON,stringify(note));
-    // },[note]);
-    const addNote=()=>{
-        const newNote ={id:Date.now()};
-        setNote([...note,newNote]);
-    }
-    const deleteNote =(id)=>{
-        setNote(note.filter(notes=>notes.id!==id));
+    const [notes,setNotes] = useState([]);
+    const constraintsRef = useRef(null)
+const addNote = () => {
+    const newNote ={
+        id:Date.now(),
+        title:"new note",
+        text:""
     };
+    setNotes((prev)=>[...prev,newNote]);
+}
+const deleteNote =(id) =>{
+    setNotes((prevNote) =>{
+      return prevNote.filter((note) => note.id != id)
+    })
+}
+
   return (
    <>
-   <div class="conatiner w-[100vw] h-[100vh] flex justify-center items-center overflow-hidden">
+   <div class="conatiner w-[100vw] h-[100vh] flex justify-center items-center overflow-hidden gap-5 flex-wrap" ref={constraintsRef}>
     <div class="addNotes fixed top-[10%] left-[10%] translate-x-[-50%] translate-y-[-50%] w-[120px] h-12 bg-black rounded-full text-zinc-50 flex justify-center items-center cursor-pointer" onClick={addNote}><h1>Notes<i class="ri-add-line"></i></h1></div>
- {
-    note.map((not,ind)=>{
-        <Note key={note.id} number={ind+1}/>
+{
+    notes.map((item,ind)=>{
+        return <Note key={ind} text={item.text} title={item.title} id={item.id}  deleteNote ={deleteNote} ref={constraintsRef} />
     })
- }
+}
     <h1 className="text-[20vw] rounded-full absolute top-[70%] left-[50%] translate-x-[-50%]
   translate-y-[-50%] z-[1] opacity-40 text">Rupesh.</h1>
    </div>
