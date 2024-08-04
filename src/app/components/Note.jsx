2256b6a35aacf243d12e5e3adbbfaa297a990428
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-08-04 10:46:03 +0530

    fix some bug

--- src/app/components/Note.jsx
+++ src/app/components/Note.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react'
 import { motion } from "framer-motion"
-const Note = ({text,id,deleteNote,title,constraintsRef}) => {
+const Note = ({text,id,deleteNote,title, color,ref}) => {
+    console.log(color);
   return (
-    <motion.div className="noteCard z-30 w-[300px] h-[300px]  p-3 rounded-2xl overflow-hidden"  drag
-    dragConstraints={constraintsRef}>
+    <motion.div className="noteCard z-30 w-[300px] h-[300px]  p-3 rounded-2xl overflow-hidden absolute
+    top-[20%] left-[10%] border-t-8 "    drag
+    dragConstraints={ref} style={{borderColor:color }}>
         <div className="top flex justify-between items-center pb-1 ">
         <input type="text" className='text-black font-bold capitalize text-[20px] w-[80%] outline-none bg-transparent' autoFocus placeholder='Title...' defaultValue={title}/>
         <i class="ri-close-fill text-[20px] cursor-pointer" onClick={()=>{
@@ -11,7 +13,7 @@ const Note = ({text,id,deleteNote,title,constraintsRef}) => {
         }}></i>
         </div>
 
-        <textarea name="" id="" className='bg-transparent text-black  w-[100%] h-[85%] outline-none pt-2' placeholder='Write here description' defaultValue={id}></textarea>
+        <textarea name="" id="" className='bg-transparent text-black  w-[100%] h-[85%] outline-none pt-2' placeholder='Write here description' defaultValue={text}></textarea>
     </motion.div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,51 @@
+
 "use client"
 import Image from "next/image";
 import 'remixicon/fonts/remixicon.css'
 import Note from "./components/Note";
 import {  useRef, useState } from "react";
-
+import { motion } from "framer-motion"
 export default function Home() {
     const [notes,setNotes] = useState([]);
-    const constraintsRef = useRef(null)
+    const constraintsRef = useRef(null);
+    const inputColor = useRef();
 const addNote = () => {
     const newNote ={
         id:Date.now(),
         title:"new note",
-        text:""
+        text:"something write here",
+        color:inputColor.current.value
     };
+    console.log(notes.color);
     setNotes((prev)=>[...prev,newNote]);
 }
 const deleteNote =(id) =>{
-    setNotes((prevNote) =>{
-      return prevNote.filter((note) => note.id != id)
-    })
+    // setNotes((prevNote) =>{
+    //   return prevNote.filter((note) => note.id !== id)
+    // })
+    const updateNote = notes.filter((elem)=>{
+        return id !== elem.id;
+    });
+    setNotes(updateNote);
 }
+
 
   return (
    <>
-   <div class="conatiner w-[100vw] h-[100vh] flex justify-center items-center overflow-hidden gap-5 flex-wrap" ref={constraintsRef}>
-    <div class="addNotes fixed top-[10%] left-[10%] translate-x-[-50%] translate-y-[-50%] w-[120px] h-12 bg-black rounded-full text-zinc-50 flex justify-center items-center cursor-pointer" onClick={addNote}><h1>Notes<i class="ri-add-line"></i></h1></div>
+   <div class="conatiner w-[100vw] h-[100vh] flex justify-center items-center  gap-5 flex-wrap overflow-hidden" ref={constraintsRef}>
+    <div class="addNotes fixed top-[10%] left-[80px] translate-x-[-50%] translate-y-[-50%] w-[120px] h-12 bg-black rounded-full text-zinc-50 flex justify-center items-center cursor-pointer z-20" onClick={addNote}><h1>Notes<i class="ri-add-line"></i></h1></div>
+    <div className=" fixed top-[20%] left-[85px]  translate-x-[-50%] translate-y-[-50%] flex color p-3 rounded-2xl">
+        <h2>Pick color </h2>
+      <input type="color" ref={inputColor} />
+    </div>
+
 {
     notes.map((item,ind)=>{
-        return <Note key={ind} text={item.text} title={item.title} id={item.id}  deleteNote ={deleteNote} ref={constraintsRef} />
+        return <Note key={ind} text={item.text} title={item.title} id={item.id}  deleteNote ={deleteNote} color={item.color} ref={constraintsRef} />
     })
 }
     <h1 className="text-[20vw] rounded-full absolute top-[70%] left-[50%] translate-x-[-50%]
-  translate-y-[-50%] z-[1] opacity-40 text">Rupesh.</h1>
+  translate-y-[-50%] z-[-1] opacity-40 text">Rupesh.</h1>
    </div>
    </>
   );
